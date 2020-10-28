3fe9bcd32b9490bcecd22331e55ef017d281fb57
sAbIz <bellett@sabiz.jp>
2020-10-28 10:58:19 +0900

    Fix not exist sysstat

--- setup.sh
+++ setup.sh
@@ -8,6 +8,8 @@ CONFIG_FILE_NAME='config.sh'
 DISK_LIST=$(lsblk -l -p -o NAME,TYPE,MOUNTPOINT |grep  -P '.+(disk|part) /(?!boot)' | cut -d" " -f1|xargs)
 ETH_INTERFACES=$(ip link| grep -E '^[0-9]+'|grep -vE 'DOWN|UNKNOWN'|cut -d':' -f 2|head -n1|xargs)
 
+type iostat > /dev/null 2>&1||(echo '[iostat] not found. Install now!'; sudo apt install sysstat)
+
 echo '# CONFIGURATIONS' > $CONFIG_FILE_NAME
 echo "DISK_LIST=($DISK_LIST)" >> $CONFIG_FILE_NAME
 echo "ETH_INTERFACES=($ETH_INTERFACES)" >> $CONFIG_FILE_NAME
